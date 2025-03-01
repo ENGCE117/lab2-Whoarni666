0667b5660c308f71c470c6a10847890e3fdd926f
Whoarni666 <sawadeegame@hotmail.com>
2025-03-01 14:20:36 +0700

    Update student_program.cpp
    lab 2 ฟังก์ชันส่งผ่านสตริง

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse( char str1[] ) ;
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
-}//end function
+    char text[ 50 ] ;
+    printf( "Enter str1: " ) ;
+    gets( text ) ;
+    
+    char *output = reverse( text ) ;
+    
+    printf( "MAIN : %s\n", output ) ;
+    return 0;
+}
+
+char* reverse( char str1[] ) {
+    int len = strlen( str1 ) ;
+    
+    for ( int i = 0; i < len / 2; i++ ) {
+        char temp = str1[ i ] ;
+        str1[i] = str1[ len - i - 1 ] ;
+        str1[ len - i - 1 ] = temp ;
+    }
+    
+    printf( "FUNC : %s\n", str1 ) ;
+    return str1;
+}
